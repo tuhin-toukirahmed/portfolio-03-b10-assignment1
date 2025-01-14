b457bf81c477c1852717362214e0fe1b797246c7
Tuhin-DEK <toukirtuhinfb002@gmail.com>
2025-01-14 16:07:10 +0600

    html and tailwind style add

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+    extend: {
+        FontFace: { 
+            'azeret-mono': {
+                family: 'Azeret Mono',
+                sources: [
+                    { src:  url('https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100..900;1,100..900&display=swap') },
+                ],
+                
+                
+            },
+    },
+    },
+    plugins: [],
+}   }
